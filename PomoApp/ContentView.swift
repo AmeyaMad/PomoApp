08c218cc96bf9b3c6f1d4d31cc6685819bd4c252
AmeyaMad <ameya.mad@gmail.com>
2024-02-15 16:22:18 -0700

    Initial Commit

---
+++ PomoApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PomoApp
+//
+//  Created by Ameya Madhugiri on 2/15/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PomoApp/PomoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PomoAppApp.swift
+//  PomoApp
+//
+//  Created by Ameya Madhugiri on 2/15/24.
+//
+
+import SwiftUI
+
+@main
+struct PomoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
