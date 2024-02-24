e95b0da13caa77a49f7bbc9db918d9c992ee5d49
AmeyaMad <ameya.mad@gmail.com>
2024-02-24 16:06:44 -0700

    [macOs] Added MenuBarTarget

---
+++ PomoAppMenuBar/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PomoAppMenuBar
+//
+//  Created by Ameya Madhugiri on 2/24/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PomoAppMenuBar/PomoAppMenuBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  PomoAppMenuBarApp.swift
+//  PomoAppMenuBar
+//
+//  Created by Ameya Madhugiri on 2/24/24.
+//
+
+import SwiftUI
+
+@main
+struct PomoAppMenuBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
